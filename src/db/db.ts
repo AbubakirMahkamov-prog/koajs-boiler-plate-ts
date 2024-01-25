c3b02c10aa07bc068f0c25e9b2f8798827863c7a
AbubakirMahkamov-prog <mahkamovabubakir65119@gmail.com>
2024-01-25 13:50:54 +0500

    some ts config and fix dev bugs

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,12 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 import * as schema from './schema'
+import { config } from "../config/index";
 
-const DB_PORT = process.env.DB_PORT;
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const DB_NAME = process.env.DB_NAME;
+const { DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT } = config
+
+
 const CONNECTION_STRING = `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 const migrationsClient = postgres(CONNECTION_STRING, {
@@ -15,7 +14,7 @@ const migrationsClient = postgres(CONNECTION_STRING, {
 });
 
 const db = drizzle(migrationsClient);
-await migrate(db, { migrationsFolder: '../../migrations' });
+await migrate(db, { migrationsFolder: './migrations' });
 
 const queryClient = postgres(CONNECTION_STRING);
 const DrizzleClient = drizzle(queryClient, {
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import Koa from 'koa'
-import { DB } from '../db/db.js'
-import { Users } from "../db/schema.js";
+
+
+import  Koa from 'koa'
+import { DB } from './db/db'
+import { Users } from "./db/schema";
+import { config } from "./config/index";
+const { PORT } = config;
 const app = new Koa();
 
-app.use(async ctx => {
+app.use(async (ctx: any) => {
   ctx.body = 'Hello World';
   // let res = await DB.insert(Users).values({
   //   name: "Abubakir"
   // }).returning()
   let res = await DB.select().from(Users);
-  console.log(res)
 });
 
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT} 🚀`)
