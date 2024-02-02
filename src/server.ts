dae62a04179d0eaf9114d41a681f3ea687a82cf4
AbubakirMahkamov-prog <mahkamovabubakir65119@gmail.com>
2024-02-02 23:59:02 +0500

    add socket

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,42 @@ import logger from 'koa-logger'
 import { config } from "./config/index";
 import { mainRouter } from "./starters/routes";
 import { dbConnect } from "./db";
+import { Server }  from 'socket.io'
+import jwt from 'jsonwebtoken';
 dbConnect()
 const { PORT } = config;
 const app = new Koa();
 
-mainRouter(app)
-app.listen(PORT, () => {
+mainRouter(app);
+
+let server = app.listen(PORT, () => {
     console.log(`App running on ${PORT} 🚀`)
 });
+
+const io = new Server(server, {
+    allowEIO3: true,
+    cors: {
+        origin: true,
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
+
+// io.use(async (socket, next) => {
+//     // user connect
+//     try {
+//         if (socket.handshake.query.token) {
+//             const token = socket.handshake.query.token;
+//             const payload = await jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+//             socket.userId = payload.user_id;
+//         }
+//     }
+    
+// })
+
+io.on('connection', async (socket) => {
+    console.log('connected ', socket);
+    socket.on("disconnect", () => { 
+        console.log('disconnected', socket)
+    });
+})
